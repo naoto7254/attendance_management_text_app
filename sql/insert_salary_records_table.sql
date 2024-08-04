a97577b4bed9054021ef932d29bbf516aced79a2
naoto takeda <naoto72725454@gmail.com>
2024-08-04 13:24:31 +0900

    test written code for registering data in the salary_record table
    to test

---
+++ sql/insert_salary_records_table.sql
@@ -0,0 +1,17 @@
+INSERT INTO salary_records (
+    part_timer_id,
+    part_timer_name,
+    part_timer_email,
+    part_timer_level,
+    shift_type,
+    date_worked,
+    generated_salary
+) VALUES (
+    1,
+    "John",
+    "john.doe@example.com",
+    3,
+    "night",
+    "2024-08-04",
+    2400
+);
