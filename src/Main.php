fb48b77171ae93d989639688e522efed9fd343c7
naoto takeda <naoto72725454@gmail.com>
2024-08-07 22:02:56 +0900

    edit Main.php
    for additional validation codes

--- src/Main.php
+++ src/Main.php
@@ -5,33 +5,46 @@ require_once('PartSalary.php');
 require_once('Calculator.php');
 require_once('Database.php');
 require_once('SalaryRecordsTable.php');
+require_once('PartTimerInfoValidator.php');
 
-$register_info = [];
+$salary_info = [];
 
 // ここは後で入力値にする必要がある>別日のシフトを登録する場合があるから
 $timezone = new DateTimeZone('Asia/Tokyo');
 $date = new DateTime('now', $timezone);
 $formatted_date = $date->format('Y-m-d');
-$register_info['date'] = $formatted_date;
+$salary_info['date_worked'] = $formatted_date;
 
 // shiftTypeについての入力
-echo "shiftTypeを['morning', 'afternoon', 'night']から入力してください";
+echo "shiftTypeを['morning', 'afternoon', 'night']から入力してください" . PHP_EOL;
 echo 'shiftType: ';
-$register_info['shift_type'] = trim(fgets(STDIN));
+$salary_info['shift_type'] = trim(fgets(STDIN));
 
-$part_timer = new PartTimer(1, 'John', 'john.doe@example.com', 3);
+// PartTimerクラスのインスタンス化
+$part_timer = new PartTimer(1, 'John', 'john.doe@example.com', 4);
 
-$calc_result = Calculator::calcSalary($part_timer, $register_info['shift_type']);
+$salary_info['part_timer_id'] = $part_timer->getId();
+$salary_info['part_timer_name'] = $part_timer->getName();
+$salary_info['part_timer_email'] = $part_timer->getEmail();
+$salary_info['part_timer_level'] = $part_timer->getLevel();
 
-$salary_info = [
-    'part_timer_id' => $part_timer->getId(),
-    'part_timer_name' => $part_timer->getName(),
-    'part_timer_email' => $part_timer->getEmail(),
-    'part_timer_level' => $part_timer->getLevel(),
-    'shift_type' => $register_info['shift_type'],
-    'date_worked' => $register_info['date'],
-    'generated_salary' => $calc_result,
-];
+// PartTimerクラスのバリデーションs
+$part_timer_validator = new PartTimerInfoValidator($part_timer);
+
+$error_detection = [];
+$error_detection[] = $part_timer_validator->validateLevel();
+
+if (in_array(false, $error_detection)) {
+    exit;
+}
+
+
+// 給料の計算
+$calc_result = Calculator::calcSalary($part_timer, $salary_info['shift_type']);
+
+$salary_info['generated_salary'] = $calc_result;
+
+
 
 Database::init();
 $link = Database::dbConnect();
